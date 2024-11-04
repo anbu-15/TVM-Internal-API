25c40826d7458d46702de9bc1dfae56dddf77dd4
Anbarasu <anbarasuannadurai7@gmail.com>
2024-11-04 14:35:26 +0530

    modified RequestMapping

--- src/main/java/com/tvm/internal/edit/controller/AppraisalController.java
+++ src/main/java/com/tvm/internal/edit/controller/AppraisalController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/app")
+@RequestMapping("/api")
 public class AppraisalController {
 
     @Autowired
