4b3b4df24840cbd7b7a2ed06ed63c6895b521650
abc <abc@example.com>
2024-12-06 10:00:23 +0530

    Education | API's for Education

--- src/main/java/com/tvm/internal/edit/repo/EducationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tvm.internal.edit.repo;
-
-import com.tvm.internal.edit.model.Education;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EducationRepository extends JpaRepository<Education,Long> {
-}
