156eaa6f7670badd3b27fd22707b632e6f12b4e3
Anbarasu <anbarasuannadurai7@gmail.com>
2024-12-13 17:01:51 +0530

    Created API for userAllDetails

--- src/main/java/com/tvm/internal/edit/controller/UserController.java
+++ src/main/java/com/tvm/internal/edit/controller/UserController.java
@@ -4,10 +4,10 @@ import com.tvm.internal.edit.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -34,5 +34,9 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login failed");
         }
         return ResponseEntity.ok(token);
+    }
+    @GetMapping("/allDetails")
+    public List<Map<String, String>> getAllUsers() {
+        return userService.getAllUserDetails();
     }
 }
